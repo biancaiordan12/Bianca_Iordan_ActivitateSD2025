c0c8ed47906ad049ad4d406dbdf3a338e0f171b3
biancaiordan12 <146763737+biancaiordan12@users.noreply.github.com>
2025-03-26 10:55:43 +0200

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -3,156 +3,172 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct StructuraMasina {
+typedef struct Masina {
     int id;
-    int nrUsi;
-    float pret;
     char* model;
-    char* numeSofer;
-    unsigned char serie;
-};
-typedef struct StructuraMasina Masina;
+    float pret;
+} Masina;
 
-struct Nod {
-    Masina masina;
+typedef struct Nod {
+    Masina info;
     struct Nod* next;
-};
-typedef struct Nod Nod;
+} Nod;
 
-Masina citireMasinaDinFisier(FILE* file) {
-    char buffer[100];
-    char sep[3] = ",\n";
-    if (!fgets(buffer, 100, file)) return (Masina) { 0 };
-
-    char* aux;
-    Masina m1;
-    aux = strtok(buffer, sep);
-    m1.id = atoi(aux);
-    m1.nrUsi = atoi(strtok(NULL, sep));
-    m1.pret = atof(strtok(NULL, sep));
-
-    aux = strtok(NULL, sep);
-    m1.model = malloc(strlen(aux) + 1);
-    strcpy(m1.model, aux);
-
-    aux = strtok(NULL, sep);
-    m1.numeSofer = malloc(strlen(aux) + 1);
-    strcpy(m1.numeSofer, aux);
-
-    m1.serie = *strtok(NULL, sep);
-    return m1;
+// Functie de creare masina
+Masina creareMasina(int id, const char* model, float pret) {
+    Masina m;
+    m.id = id;
+    m.model = (char*)malloc(strlen(model) + 1);
+    strcpy(m.model, model);
+    m.pret = pret;
+    return m;
 }
 
-void afisareMasina(Masina masina) {
-    printf("Id: %d\n", masina.id);
-    printf("Nr. usi : %d\n", masina.nrUsi);
-    printf("Pret: %.2f\n", masina.pret);
-    printf("Model: %s\n", masina.model);
-    printf("Nume sofer: %s\n", masina.numeSofer);
-    printf("Serie: %c\n\n", masina.serie);
+// Functie de afisare masina
+void afisareMasina(Masina m) {
+    printf("ID: %d | Model: %s | Pret: %.2f\n", m.id, m.model, m.pret);
 }
 
-void afisareListaMasini(Nod* cap) {
-    while (cap) {
-        afisareMasina(cap->masina);
-        cap = cap->next;
-    }
-}
-
-Nod* adaugaMasinaInLista(Nod* cap, Masina masinaNoua) {
+// Inserare sortata crescator dupa pret
+void inserareSortata(Nod** cap, Masina m) {
     Nod* nou = (Nod*)malloc(sizeof(Nod));
-    nou->masina = masinaNoua;
+    nou->info = creareMasina(m.id, m.model, m.pret);
     nou->next = NULL;
-    if (!cap) return nou;
-    Nod* temp = cap;
-    while (temp->next) temp = temp->next;
-    temp->next = nou;
-    return cap;
-}
 
-Nod* adaugaLaInceputInLista(Nod* cap, Masina masinaNoua) {
-    Nod* nou = (Nod*)malloc(sizeof(Nod));
-    nou->masina = masinaNoua;
-    nou->next = cap;
-    return nou;
-}
-
-Nod* citireListaMasiniDinFisier(const char* numeFisier) {
-    FILE* file = fopen(numeFisier, "r");
-    if (!file) return NULL;
-    Nod* cap = NULL;
-    while (!feof(file)) {
-        Masina m = citireMasinaDinFisier(file);
-        cap = adaugaMasinaInLista(cap, m);
+    if (*cap == NULL || m.pret < (*cap)->info.pret) {
+        nou->next = *cap;
+        *cap = nou;
     }
-    fclose(file);
-    return cap;
+    else {
+        Nod* curent = *cap;
+        while (curent->next && curent->next->info.pret < m.pret)
+            curent = curent->next;
+        nou->next = curent->next;
+        curent->next = nou;
+    }
 }
 
-void dezalocareListaMasini(Nod* cap) {
+// Afisare lista
+void afisareLista(Nod* cap) {
     while (cap) {
-        Nod* temp = cap;
+        afisareMasina(cap->info);
         cap = cap->next;
-        free(temp->masina.model);
-        free(temp->masina.numeSofer);
+    }
+}
+
+// 1. Stergere nod la o pozitie data
+void stergereLaPozitie(Nod** cap, int pozitie) {
+    if (*cap == NULL || pozitie < 0) return;
+
+    Nod* temp = *cap;
+
+    if (pozitie == 0) {
+        *cap = temp->next;
+        free(temp->info.model);
+        free(temp);
+        return;
+    }
+
+    for (int i = 0; i < pozitie - 1 && temp != NULL; i++)
+        temp = temp->next;
+
+    if (temp == NULL || temp->next == NULL) return;
+
+    Nod* nodDeSters = temp->next;
+    temp->next = nodDeSters->next;
+    free(nodDeSters->info.model);
+    free(nodDeSters);
+}
+
+// 2. Inserare sortata – deja implementata mai sus
+
+// 3. Salvare in vector elemente care au pret > valoare
+Masina* salveazaInVector(Nod* cap, int* dim, float prag) {
+    *dim = 0;
+    Nod* temp = cap;
+
+    while (temp) {
+        if (temp->info.pret > prag)
+            (*dim)++;
+        temp = temp->next;
+    }
+
+    if (*dim == 0) return NULL;
+
+    Masina* vector = (Masina*)malloc(sizeof(Masina) * (*dim));
+    int k = 0;
+    temp = cap;
+
+    while (temp) {
+        if (temp->info.pret > prag) {
+            vector[k] = creareMasina(temp->info.id, temp->info.model, temp->info.pret);
+            k++;
+        }
+        temp = temp->next;
+    }
+    return vector;
+}
+
+// 4. Interschimbare elemente de pe doua pozitii
+void interschimbarePozitii(Nod* cap, int p1, int p2) {
+    if (p1 == p2) return;
+
+    Nod* nod1 = NULL;
+    Nod* nod2 = NULL;
+    int index = 0;
+
+    while (cap) {
+        if (index == p1) nod1 = cap;
+        if (index == p2) nod2 = cap;
+        cap = cap->next;
+        index++;
+    }
+
+    if (nod1 && nod2) {
+        Masina temp = nod1->info;
+        nod1->info = nod2->info;
+        nod2->info = temp;
+    }
+}
+
+// Dezalocare
+void dezalocareLista(Nod** cap) {
+    while (*cap) {
+        Nod* temp = *cap;
+        *cap = (*cap)->next;
+        free(temp->info.model);
         free(temp);
     }
-}
-
-float calculeazaPretMediu(Nod* cap) {
-    if (!cap) return 0;
-    float suma = 0;
-    int nr = 0;
-    while (cap) {
-        suma += cap->masina.pret;
-        nr++;
-        cap = cap->next;
-    }
-    return suma / nr;
-}
-
-Nod* stergeMasiniDinSeria(Nod* cap, char serieCautata) {
-    while (cap && cap->masina.serie == serieCautata) {
-        Nod* temp = cap;
-        cap = cap->next;
-        free(temp->masina.model);
-        free(temp->masina.numeSofer);
-        free(temp);
-    }
-    Nod* temp = cap;
-    while (temp && temp->next) {
-        if (temp->next->masina.serie == serieCautata) {
-            Nod* deSters = temp->next;
-            temp->next = deSters->next;
-            free(deSters->masina.model);
-            free(deSters->masina.numeSofer);
-            free(deSters);
-        }
-        else {
-            temp = temp->next;
-        }
-    }
-    return cap;
-}
-
-float calculeazaPretulMasinilorUnuiSofer(Nod* cap, const char* numeSofer) {
-    float suma = 0;
-    while (cap) {
-        if (strcmp(cap->masina.numeSofer, numeSofer) == 0) {
-            suma += cap->masina.pret;
-        }
-        cap = cap->next;
-    }
-    return suma;
 }
 
 int main() {
-    Nod* listaMasini = citireListaMasiniDinFisier("masini.txt");
-    afisareListaMasini(listaMasini);
-    printf("Pret mediu: %.2f\n", calculeazaPretMediu(listaMasini));
-    listaMasini = stergeMasiniDinSeria(listaMasini, 'A');
-    afisareListaMasini(listaMasini);
-    printf("Pret total masini sofer X: %.2f\n", calculeazaPretulMasinilorUnuiSofer(listaMasini, "SoferX"));
-    dezalocareListaMasini(listaMasini);
+    Nod* lista = NULL;
+
+    inserareSortata(&lista, creareMasina(1, "Dacia", 5000));
+    inserareSortata(&lista, creareMasina(2, "BMW", 20000));
+    inserareSortata(&lista, creareMasina(3, "Audi", 15000));
+    inserareSortata(&lista, creareMasina(4, "Mercedes", 25000));
+
+    printf("Lista sortata:\n");
+    afisareLista(lista);
+
+    printf("\nStergem elementul de pe pozitia 2:\n");
+    stergereLaPozitie(&lista, 2);
+    afisareLista(lista);
+
+    printf("\nElemente cu pret > 10000:\n");
+    int dim = 0;
+    Masina* v = salveazaInVector(lista, &dim, 10000);
+    for (int i = 0; i < dim; i++) {
+        afisareMasina(v[i]);
+        free(v[i].model);
+    }
+    free(v);
+
+    printf("\nInterschimbam pozitiile 0 si 1:\n");
+    interschimbarePozitii(lista, 0, 1);
+    afisareLista(lista);
+
+    dezalocareLista(&lista);
     return 0;
 }
